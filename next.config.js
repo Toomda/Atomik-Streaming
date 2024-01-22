d8fe6f1ce0eb9e4ae14f132c801a8297a07c23e4
Tom Lucksted <tom.lucksted@wysa.de>
2024-01-22 08:38:49 +0100

    add AWS Info to Image Domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost:5000', 'localhost'],
+    domains: [
+      "localhost:5000",
+      "localhost",
+      "3.66.166.93",
+      "ip-172-31-16-169.eu-central-1.compute.internal",
+    ],
   },
 };
 
