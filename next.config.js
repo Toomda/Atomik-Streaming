ce2a81834bc45074606b2cc309c19930ee69402c
Tom Lucksted <tom.lucksted@wysa.de>
2024-01-22 15:56:18 +0100

    change image domains

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "localhost:5000",
-      "localhost",
-      "3.66.166.93",
-      "ip-172-31-16-169.eu-central-1.compute.internal",
-    ],
+    domains: ["localhost:5000", "localhost", "atomik-streaming.tv"],
   },
 };
 
